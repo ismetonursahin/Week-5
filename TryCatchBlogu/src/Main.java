c7caa46e6e7dfa36c14059b270933f6b54a17b5d
İsmet Onur ŞAHİN <110236136+ismetonursahin@users.noreply.github.com>
2024-01-06 00:17:33 +0300

    Try-Catch Blogu

--- TryCatchBlogu/src/Main.java
+++ TryCatchBlogu/src/Main.java
@@ -1,17 +1,30 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+    public static void  print(int arr[], int index) throws CustomException{
+        if (index >= arr.length){
+            throw  new CustomException("");
+        }
+        System.out.println(arr[index]);
+    }
+
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int index ;
-
-        int [] arr = {1,2,3,4,5,6,7,8,9,0};
+        System.out.print("index gir : ");
+        int index = input.nextInt();
+        int [] arr = new int[10];
 
           try {
-            index = input.nextInt();
-            System.out.println(arr[index]);
-          }catch (ArrayIndexOutOfBoundsException e){
+            print(arr,index);
+          }catch (CustomException e){
               System.out.println(e.getMessage());
+          }catch (Exception e){
+              System.out.println("Sayı girmelisin.");
+          }finally {
+              System.out.println("Kod sona geldi");
           }
     }
 }
